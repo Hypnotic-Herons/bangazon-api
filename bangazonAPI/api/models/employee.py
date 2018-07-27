96df337659bd1b8d3ff9e64e1078fedd81e271a3
Levi Schubert <me@levischubert.com>
2018-07-27 15:29:57 -0500

    committing to check out

---
+++ bangazonAPI/api/models/employee.py
@@ -0,0 +1,15 @@
+from django.db import models
+from safedelete.models import SafeDeleteModel
+from .department import Department
+'''
+Author: Levi Schubert
+'''
+
+class Employee(SafeDeleteModel):
+	first_name = models.CharField(max_length=100)
+	last_name = models.CharField(max_length=100)
+	supervisor = models.BooleanField(blank=True, default=False)
+	department = models.ForeignKey(Department)
+
+	class Meta:
+		db_tables = "employees"
