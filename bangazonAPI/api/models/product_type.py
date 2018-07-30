beee6637679adf33d2a85aeb20bd506970b6f16d
Deanna Vickers <deanna2000@gmail.com>
2018-07-30 09:17:46 -0500

    added docstrings

--- bangazonAPI/api/models/product_type.py
+++ bangazonAPI/api/models/product_type.py
@@ -4,6 +4,7 @@
 from django.db import models
 
 class ProductType(models.Model):
+	"""Define Product Type table."""
 	category = models.CharField(max_length=100, default="")
 	class Meta:
 		db_table = "product_type"
