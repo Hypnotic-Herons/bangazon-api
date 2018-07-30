480e998ccd707164ee9f02f9d25ea952b9f81c37
Deanna Vickers <deanna2000@gmail.com>
2018-07-30 10:47:13 -0500

    cleaned up

--- bangazonAPI/api/models/computer.py
+++ bangazonAPI/api/models/computer.py
@@ -7,9 +7,9 @@ from django.db import models
 class Computer(SafeDeleteModel):
 	"""Define the table for computers - each person has one computer.
     """
-	purchase_date = models.CharField(max_length=100, default="")
+	purchase_date = models.CharField(max_length=100)
 	ValidationError: ['This field is required.']
-	decommission_date = models.CharField(max_length=100, default="", required=false)
+	decommission_date = models.CharField(max_length=100, default="")
 	employeeID = models.ForeignKey('Employee', on_delete=models.CASCADE)
 
 	class Meta:
