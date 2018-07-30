b1e70f2424c78ba650b7ebc9a22cf806bc682971
Meghan Debity <mdebity@gmail.com>
2018-07-30 11:24:51 -0500

    checked my file against ticket, moving on to serializer

--- bangazonAPI/api/models/payment_type.py
+++ bangazonAPI/api/models/payment_type.py
@@ -7,11 +7,13 @@ from .customer import Customer
 
 '''
 Author: Meghan Debity
+Purpose: To create and manage the payment type table in the database.
 '''
 
 class Payment_Type(SafeDeleteModel):
 	payment_type = models.CharField(max_length=100)
 	customer = models.ForeignKey('Customer', on_delete=models.CASCADE)
+	# order = models.ForeignKey('Order', on_delete=models.CASCADE)
 
 	class Meta:
 		db_table = "payment type"
