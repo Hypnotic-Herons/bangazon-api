710a0859e479d5ac533bfee6cbc3fb466ff4e092
Meghan Debity <mdebity@gmail.com>
2018-07-31 09:59:42 -0500

    worked on the orders table model

---
+++ bangazonAPI/api/models/orders.py
@@ -0,0 +1,19 @@
+from django.db import models
+from safedelete.models import SafeDeleteModel, SOFT_DELETE_CASCADE
+from .customer import Customer
+from .payment_type import PaymentType
+
+
+
+class Order(SafeDeleteModel):
+	'''
+	Author: Meghan Debity
+	Purpose: Model for Order Table
+	'''
+	_safedelete_policy = SOFT_DELETE_CASCADE
+	order_complete = models.BooleanField(default=False)
+	customer_id = models.ForeignKey('Customer', on_delete=models.CASCADE)
+	payment_id = models.ForeignKey('PaymentType', on_delete=models.CASCADE)
+
+	class Meta:
+		db_table = "order"
