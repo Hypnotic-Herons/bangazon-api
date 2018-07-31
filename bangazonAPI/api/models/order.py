7f645c4e1e6ea08e1ea1846d0557cb21456c1bf3
Levi Schubert <me@levischubert.com>
2018-07-31 14:38:29 -0500

    nested product details completed

--- bangazonAPI/api/models/order.py
+++ bangazonAPI/api/models/order.py
@@ -3,6 +3,7 @@ from safedelete.models import SafeDeleteModel, SOFT_DELETE_CASCADE
 from .customer import Customer
 from .product import Product
 from .payment_type import PaymentType
+# from .product_order_join import ProductOrderJoin
 
 
 
@@ -12,10 +13,9 @@ class Order(SafeDeleteModel):
 	Purpose: Model for Order Table
 	'''
 	_safedelete_policy = SOFT_DELETE_CASCADE
-	order_complete = models.BooleanField(default=False)
-	products_on_order = models.ManyToManyField(Product,blank=True)
 	customer_id = models.ForeignKey('Customer', on_delete=models.CASCADE)
-	payment_id = models.ForeignKey('PaymentType', on_delete=models.CASCADE)
+	payment_id = models.ForeignKey('PaymentType', on_delete=models.CASCADE, null=True)
+	products = models.ManyToManyField(through='ProductOrderJoin', to='Product')
 
 	class Meta:
 		db_table = "order"
